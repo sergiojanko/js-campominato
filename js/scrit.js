31a3569e2263f413eab2912a13c469f385de4cfa
sergiojanko <janko.sergio1998@gmail.com>
2021-07-13 15:48:01 +0200

    boilerplate

---
+++ js/scrit.js
@@ -0,0 +1,9 @@
+/* 
+Il computer deve generare 16 numeri casuali tra 1 e 100, queste saranno le nostre bombe.
+I numeri delle bombe non possono essere duplicati (i 16 numeri devono essere tutti diversi)
+Il giocatore, deve cercare di non prendere le bombe. Gli chiederemo 100 - 16 volte di scegliere un numero, uno alla volta, sempre compreso tra 1 e 100.
+L'utente non può inserire 2 volte lo stesso numero
+Ogni  volta che l'utente sceglie un numero che non è presente tra le bombe, guadagna un punto e poi gli chiediamo un altro numero.
+Se il numero scelto dall'utente è presente tra i numeri bomba, la partita termina.
+Quando la partita termina, comunichiamo all'utente il suo punteggio.
+*/
